2ac0e187552e535654760c1b41072f6a5402ec64
Mateusz Zajac <mateusz.zajac@doverfs.com>
2026-08-05 08:20:58 +0200

    add simple ut

--- todo/todo_test.go
+++ todo/todo_test.go
@@ -1,1 +1,20 @@
 package todo
+
+import (
+	"testing"
+)
+
+func TestAddingTaskToList(t *testing.T) {
+	t.Parallel()
+
+	// arrange
+	var list List
+	
+	// act
+	list.Add("task 1")
+
+	// assert
+	if len(list) != 1 {
+		t.Errorf("got %d but expected 1", len(list))
+	}
+}
