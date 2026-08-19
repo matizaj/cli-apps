4d8db3be128baa215d4f0c81dcd39f5c5d29e420
Mateusz <matzaj101@gmail.com>
2026-08-19 11:59:13 +0200

    add function to calculate statistics

--- performance/colStat/csv.go
+++ performance/colStat/csv.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type statsFunc func(data []float64) float64
+
+func sum(data []float64) float64 {
+	sum:=0.0
+
+	for _, v := range data {
+		sum +=v
+	}
+
+	return sum
+}
+
+func avg(data []float64) float64 {
+	return sum(data)/float64(len(data))
+}
+
+func csv2float(r io.Reader, column int)([]float64, error) {
+	cr := csv.NewReader(r)
+
+	// adjusting for 0 base index
+	column--
+
+	allData, err := cr.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("cannot read data from file: %w\n", err)
+	}
+
+	var data []float64
+	for i, row:=range allData {
+		if i == 0 {
+			continue
+		}
+		if len(row) <= column {
+			// file dont have that many columns
+			return nil, fmt.Errorf("%w file has only %d colums", ErrInvalidColumn, len(row))
+		}
+
+		// try convert data
+		v, err:=strconv.ParseFloat(row[column], 64)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+		}
+		data = append(data, v)
+	}
+	return data, nil
+
+}
