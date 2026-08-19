2500839fa3f72fa1e5780ba80c3003c2828a9b55
Mateusz <matzaj101@gmail.com>
2026-08-19 12:04:03 +0200

    add parsing cmd ln args

--- performance/colStat/main.go
+++ performance/colStat/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	fmt.Println("ColStat")
+
+	op:=flag.String("op", "", "operation to perform")
+	col:=flag.Int("col", 0, "CSV column number on which execute the operation")
+
+	flag.Parse()
+
+	if err := run(flag.Args(), *op, *col, os.Stdout); err!= nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
+
+func run(filenames []string, op string, col int, out io.Writer) error {
+	return nil
 }
