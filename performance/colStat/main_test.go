0badc848b4356dbffc173eae1829d671ccb4d5d2
Mateusz <matzaj101@gmail.com>
2026-08-20 22:24:58 +0200

    add benchmark test

--- performance/colStat/main_test.go
+++ performance/colStat/main_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"errors"
+	"io"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -45,5 +47,20 @@ func TestRun(t *testing.T) {
 				t.Errorf("expected %q but got %q", tc.exp, &buffer)
 			}
 		})
+	}
+}
+
+func BenchmarkRun(b *testing.B) {
+	filenames, err := filepath.Glob(".\\testdata\\benchmark\\*.csv")
+	if err!= nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i:=0; i<b.N; i++ {
+		if err := run(filenames, "a", 2, io.Discard); err != nil {
+			b.Error(err)
+		}
 	}
 }
