782489f613a49c30614d05ea2e0bfbb09eb5a0b9
Mateusz <matzaj101@gmail.com>
2026-08-24 10:51:49 +0200

    example: concurrently reading files

--- performance/colStat/example/main.go
+++ performance/colStat/example/main.go
@@ -5,31 +5,59 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 )
 
 func main() {
 	fmt.Println("reading from file..")
+	var wg sync.WaitGroup
 	flag.Parse()
 	args := flag.Args()
 	
 	fmt.Printf("Args: %s\n", args)
-	buffer := make([]byte, 4096)    
+	resultChan := make(chan string)
+	doneChan := make(chan struct{})
+
+	
+	
 
 	for _, fname := range args {
-		file, err := os.Open(fname)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cannot open file: %v", err)
-			os.Exit(1)
-		}
-		
-		for {
-			bytes, err := file.Read(buffer)
-			if bytes >0 {
-				fmt.Printf("%s ",string(buffer[:bytes]))
+		wg.Add(1)
+		go func(fname string) {
+			buffer := make([]byte, 4096)
+			defer wg.Done()
+			file, err := os.Open(fname)
+			
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "cannot open file: %v", err)
+				os.Exit(1)
 			}
-			if err == io.EOF {
-				break
+			defer file.Close()
+			for {
+				bytes, err := file.Read(buffer)
+				if bytes >0 {
+					resultChan <- string(buffer[:bytes])
+				}
+				if err == io.EOF {
+					break
+				}
 			}
+			
+		}(fname)
+	}
+	
+	go func() {
+		wg.Wait()
+		close(doneChan)
+	}()
+
+	for {
+		select {
+			case data:= <-resultChan: 
+				fmt.Println(data)
+			case <-doneChan:
+				return
 		}
 	}
 	
