1fcdc2121d9e8e0f86bfc4ceb65f91d4d2b1db37
Mateusz <matzaj101@gmail.com>
2026-08-26 09:44:01 +0200

    add initial version

--- processes/goci/main.go
+++ processes/goci/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
 
 func main() {
 	fmt.Println("..::GOCI::..")
+	
+	proj := flag.String("p","", "project directory path")
+	flag.Parse()
+
+	if err := run(*proj, os.Stdout);err!=nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+
+func run(proj string, out io.Writer) error {
+	if proj == "" {
+		return fmt.Errorf("project directory is required")
+	}
+	args := []string{"build", ".", "error"}
+
+	cmd := exec.Command("go", args...)
+	cmd.Dir = proj
+
+	if err:= cmd.Run(); err!= nil {
+		return fmt.Errorf("'go build' failed: %s", err)
+	}
+
+	_, err := fmt.Fprintln(out, "Go Build: SUCCESS")
+
+	return err
 }
