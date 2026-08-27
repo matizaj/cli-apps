ef4db0a6541b5bfeca76a0623d118af01b2e16bb
Mateusz <matzaj101@gmail.com>
2026-08-27 11:27:03 +0200

    define step pipeline

---
+++ processes/goci/step.go
@@ -0,0 +1,28 @@
+package main
+
+import "os/exec"
+
+type step struct {
+	name    string
+	exe     string
+	message string
+	proj    string
+	args    []string
+}
+
+func newStep(name, exe, message, proj string, args []string) step {
+	return step{name, exe, message, proj, args}
+}
+
+func (s step) execute() (string, error) {
+	cmd := exec.Command(s.exe, s.args...)
+	cmd.Dir = s.proj
+	if err := cmd.Run(); err!= nil {
+		return "", &stepErr{
+			step: s.name,
+			msg: "failed to execute",
+			cause: err,
+		}
+	}
+	return s.message, nil
+}
