02faa2577774e3ad7c2d50e89f15cb628a5864dd
Mateusz <matzaj101@gmail.com>
2026-08-31 13:09:19 +0200

    handle signal stop notification

--- processes/goci/main.go
+++ processes/goci/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -55,17 +57,40 @@ func run(proj string, out io.Writer) error {
 		[]string{"push", "origin", "master"},
 		time.Second*3,
 	)
-	
-	for _, s := range pipeline {
+
+	sig:=make(chan os.Signal, 1)
+	errChan:= make(chan error)
+	doneChan:=make(chan struct{})
+
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		for _, s := range pipeline {
 		msg, err := s.execute()
 		if err != nil {
-			return err
+			errChan <- err
+			return
 		}
 
 		_, err = fmt.Fprintln(out, msg)
 		if err != nil {
-			return err
+			errChan<-err
+			return
 		}
 	}
-	return nil
+	close(doneChan)
+	}()
+
+
+	for {
+		select {
+			case err:= <-errChan:
+				return err
+			case rec := <-sig:
+				signal.Stop(sig)
+				return fmt.Errorf("%s: exiting %w", rec, ErrSignal)
+			case <-doneChan:
+				return nil
+		}
+	}
+	
 }
