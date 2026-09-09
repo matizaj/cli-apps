904ea7928f67cf79652a07425a8499d70e722267
Mateusz <matzaj101@gmail.com>
2026-09-09 14:51:19 +0200

    add server skeleton

---
+++ apis/todoServer/handlers.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w,r)
+		return
+	}
+	content := "todo server api"
+	replyTextContent(w,r,http.StatusOK, content)
+}
+
+func replyTextContent(w http.ResponseWriter, r *http.Request, status int, content string) {
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(status)
+	w.Write([]byte(content))
+}
