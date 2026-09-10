5dda2b9077f5c15bf7a5160853af3bcd23b4b3c1
Mateusz <matzaj101@gmail.com>
2026-09-10 15:33:11 +0200

    add func to handle json content

--- apis/todoServer/server.go
+++ apis/todoServer/server.go
@@ -1,9 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 func newMux(todoFile string) http.Handler {
 	m := http.NewServeMux()
 	m.HandleFunc("/", rootHandler)
 	return m
+}
+
+func replyJsonContent(w http.ResponseWriter, r *http.Request, status int, resp *todoResponse) {
+	body, err := json.Marshal(resp)
+	if err!=nil {
+		replyError(w,r,http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(body)
+}
+
+func replyError(w http.ResponseWriter, r *http.Request, status int, msg string) {
+	log.Printf("%s %s: Error: %d %s", r.URL, r.Method, status, msg)
+	http.Error(w, http.StatusText(status), status)
 }
