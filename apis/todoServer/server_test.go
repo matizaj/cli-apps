3e1b412f2400d554a2a155a9e7131092e2c3e152
Mateusz <matzaj101@gmail.com>
2026-09-10 12:41:39 +0200

    ut for root path & not found

--- apis/todoServer/server_test.go
+++ apis/todoServer/server_test.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
-
-	"golang.org/x/tools/go/analysis/passes/defers"
 )
 
 func setupApi(t *testing.T) (string, func()) {
@@ -31,4 +31,35 @@ func TestGet(t *testing.T) {
 
 	url, cleanup := setupApi(t)
 	defer cleanup()
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var (
+				body []byte
+				err error
+			)
+			resp, err := http.Get(url+tc.path)
+			if err != nil {
+				t.Error(err)
+			}
+
+			defer resp.Body.Close()
+
+			if tc.expCode != resp.StatusCode {
+				t.Fatalf("expected %d but got %d", tc.expCode, resp.StatusCode)
+			}
+
+			switch {
+			case strings.Contains(resp.Header.Get("Content-Type"), "text/plain"):
+				if body, err = io.ReadAll(resp.Body); err != nil {
+					t.Error(err)
+				}
+				if !strings.Contains(string(body), tc.expContent) {
+					t.Errorf("expected body %s but got %s", tc.expContent, string(body))
+				} 
+				default:
+					t.Errorf("unsupported content-type")
+			}
+		})
+	}
 }
