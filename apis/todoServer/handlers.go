294f5f1fa10a1bf82c36f4f49a1e494090f3db29
Mateusz <matzaj101@gmail.com>
2026-09-11 10:34:23 +0200

    add /todo router

--- apis/todoServer/handlers.go
+++ apis/todoServer/handlers.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
 	"matizaj/cli-apps/todo"
 	"net/http"
 	"sync"
 )
 
+var (
+	ErrNotFound = errors.New("not found")
+	ErrInvalidData = errors.New("invalid input")
+)
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w,r)
@@ -44,6 +49,27 @@ func todoRouter(todoFile string, l sync.Locker) http.HandlerFunc {
 			}
 			return 
 		}
-		
+
+		id, err := validateId(r.URL.Path, list)
+		if err != nil {
+			if errors.Is(err, ErrNotFound) {
+				replyError(w,r,http.StatusNotFound, err.Error())
+				return
+			}
+			replyError(w,r,http.StatusBadRequest, err.Error())
+				return
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			getOneHandler(w,r,list, id)
+		case http.MethodDelete:
+			deleteHandler(w,r,list, id, todoFile)
+		case http.MethodPatch:
+			patchHandler(w,r,list,id, todoFile)
+		default:
+			message:= "Method not supported"
+			replyError(w,r,http.StatusMethodNotAllowed, message)
+		}		
 	}
 }
