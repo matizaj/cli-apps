e1d424c2988caceb6e37173da3a839fe6780f9e5
Mateusz <matzaj101@gmail.com>
2026-09-14 11:26:15 +0200

    add client get items action

---
+++ apis/todoClient/cmd/client.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+var (
+	ErrConnection = errors.New("connection error")
+	ErrNotForund = errors.New("not found")
+	ErrInvalidResponse = errors.New("invalid response")
+	ErrInvalid = errors.New("invalid data")
+	ErrNotNumber = errors.New("not a number")
+)
+
+type item struct {
+	Task string
+	Done bool
+	CreatedAt time.Time
+	CompletedAt time.Time
+}
+
+type response struct {
+	Results []item	`json:results`
+	Date int	`json:date`
+	Totalresults int	`json:total_results`
+}
+
+func newClient() *http.Client {
+	return &http.Client{
+		Timeout: 10*time.Second,
+	}
+}
+
+func getItems(url string) ([]item, error) {
+	r, err := newClient().Get(url)
+	if err !=nil {
+		return nil, fmt.Errorf("%w: %s", ErrConnection, err)
+	}
+
+	defer r.Body.Close()
+
+	if r.StatusCode!= http.StatusOK {
+		msg, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to process request %s", string(msg))
+		}
+		err = ErrInvalidResponse
+		if r.StatusCode == http.StatusNotFound {
+			err=ErrNotForund
+		}
+		return nil, fmt.Errorf("%w: %s", err, msg)
+		
+	}
+
+	var resp response
+	if err := json.NewDecoder(r.Body).Decode(&resp); err != nil {
+		return nil, err
+	}
+}
