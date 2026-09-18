a68f4ee0194ca249ccbc0883773333e0ba357ccf
Mateusz <matzaj101@gmail.com>
2026-09-18 15:08:45 +0200

    add state contants

--- interactive/pomo/pomodoro/internal.go
+++ interactive/pomo/pomodoro/internal.go
@@ -1,7 +1,17 @@
 package pomodoro
 
+//category const
 const (
 	CategoryPomodoro="Pomodoro"
 	CategoryShortBreak="ShortBreak"
 	CategoryLongBreak="LongBreak"
+)
+
+//state const
+const (
+	StateNotStarted = iota
+	StateRunning
+	StatePaused
+	StateDone
+	StateCancelled
 )
